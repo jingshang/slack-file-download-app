6e45a8bae919e1277f61d0a30ffad18047a853ce
kentaro_inoue <jingshang1218@gmail.com>
2021-05-05 16:17:55 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,160 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+using WebAPIClient;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		private static readonly HttpClient client = new HttpClient();
+
+		public static async Task Main(string[] args)
+		{
+			// 設定ファイル読込
+			var query_param_dic = ReadSetting();
+			if (query_param_dic == null) return;
+			var download_dir = query_param_dic["download_dir"].ToString();
+			query_param_dic.Remove("download_dir");
+			var authkey = query_param_dic["authkey"].ToString();
+			query_param_dic.Remove("authkey");
+
+			// 検索条件作成
+			var query_param_txt = await new FormUrlEncodedContent(query_param_dic).ReadAsStringAsync();
+
+
+			// Headerの設定
+			client.DefaultRequestHeaders.Accept.Clear();
+			client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/x-www-form-urlencoded+json"));
+			client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", authkey);
+
+			// Slackからデータ取得
+			var message = new HttpRequestMessage(HttpMethod.Post, $"https://slack.com/api/search.files?{query_param_txt}");
+			var slack_response1 = await client.SendAsync(message, HttpCompletionOption.ResponseHeadersRead);
+			var m = await slack_response1.Content.ReadAsStreamAsync();
+
+			// Jsonからオブジェクトに変換
+			var slack = await JsonSerializer.DeserializeAsync<Rootobject>(m);
+
+			// 一致したデータに対する処理
+			foreach (var matche in slack.files.matches)
+			{
+				Console.WriteLine(matche.name);
+				Console.WriteLine(matche.url_private_download);
+				if(matche.url_private_download==null)
+				{
+					Console.WriteLine("ダウンロード対象が存在しません。スキップします。");
+					continue;
+				}
+				Console.WriteLine("ダウンロード中...");
+				using (var request = new HttpRequestMessage(HttpMethod.Get, new Uri(matche.url_private_download)))
+				using (var response = await client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead))
+				{
+					if (response.StatusCode == System.Net.HttpStatusCode.OK)
+					{
+						var fullpath = System.IO.Path.Combine(download_dir, matche.name);
+						using (var content = response.Content)
+						using (var stream = await content.ReadAsStreamAsync())
+						using (var fileStream = new FileStream(fullpath, FileMode.Create, FileAccess.Write, FileShare.None))
+						{
+							stream.CopyTo(fileStream);
+						}
+					}
+				}
+				Console.WriteLine("ダウンロード完了");
+				Console.WriteLine();
+			}
+		}
+
+		private static Dictionary<string, string> ReadSetting()
+		{
+			IConfiguration configuration = new ConfigurationBuilder()
+			  .SetBasePath(Directory.GetCurrentDirectory())
+			  .AddJsonFile("appsettings.json", true, true)
+			  .Build();
+
+			IConfigurationSection section = configuration.GetSection("search");
+
+			Console.WriteLine("※appsettings.jsonはUTF-8で保存してください。");
+			Console.WriteLine();
+			string query = section["query"];
+			Console.WriteLine("【検索クエリ】");
+			Console.WriteLine(string.IsNullOrWhiteSpace(query)?"指定してください。": query);
+			Console.WriteLine();
+			
+			var sort = section["sort"];
+			Console.WriteLine("【検索結果ソートキー】");
+			Console.WriteLine(string.IsNullOrWhiteSpace(sort) ? "指定してください。" : sort);
+			Console.WriteLine();
+
+			var sort_dir = section["sort_dir"];
+			Console.WriteLine("【検索結果並び順】");
+			Console.WriteLine(string.IsNullOrWhiteSpace(sort_dir) ? "指定してください。" : sort_dir);
+			Console.WriteLine();
+
+			var download_dir = section["download_dir"];
+			Console.WriteLine("【ダウンロードフォルダ】");
+			Console.WriteLine(string.IsNullOrWhiteSpace(download_dir) ? "指定してください。" : download_dir);
+			if (!string.IsNullOrWhiteSpace(download_dir) && !Directory.Exists(download_dir))
+			{
+				Console.WriteLine("※ダウンロードフォルダは自動的に作成されます。");
+			}
+			Console.WriteLine("※Windowsの場合、異なるドライブには保存できません。");
+			Console.WriteLine();
+
+			var authkey = section["authkey"];
+			Console.WriteLine("【authkey】");
+			Console.WriteLine(string.IsNullOrWhiteSpace(authkey) ? "指定してください。" : authkey);
+
+			Console.WriteLine();
+			Console.WriteLine();
+			if (!string.IsNullOrWhiteSpace(query) 
+				&& !string.IsNullOrWhiteSpace(sort_dir)
+				&& !string.IsNullOrWhiteSpace(sort)
+				&& !string.IsNullOrWhiteSpace(download_dir)
+				&& !string.IsNullOrWhiteSpace(authkey))
+			{
+				Console.WriteLine("ダウンロード開始しますか？ [y] + Enter");
+				Console.WriteLine("キャンセルはEnter押下。");
+				Console.WriteLine();
+			}
+			else
+			{
+				Console.WriteLine("設定ファイル（appsettings.json）が不正です");
+				Console.WriteLine("終了はEnter押下。");
+				Console.WriteLine();
+				Console.ReadLine();
+				return null;
+			}
+
+			var input = Console.ReadLine();
+			if(input == null || 0 == input.Length || "y" != input.ToString().ToLower())
+			{
+				return null;
+			}
+
+			if (!Directory.Exists(download_dir))
+			{
+				Directory.CreateDirectory(download_dir);
+			}
+
+
+			return new Dictionary<string, string>()
+			{
+				{ "query", query},
+				{ "sort", sort },
+				{ "sort_dir", sort_dir },
+				{ "pretty", "1" },
+				{ "download_dir", download_dir },
+				{ "authkey", authkey },
+			};
+
+		}
+	}
+
+}
---
+++ slack.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace WebAPIClient
+{
+
+	public class Rootobject
+	{
+		public bool ok { get; set; }
+		public string query { get; set; }
+		public Files files { get; set; }
+	}
+
+	public class Files
+	{
+		public int total { get; set; }
+		public Pagination pagination { get; set; }
+		public Paging paging { get; set; }
+		public Match[] matches { get; set; }
+	}
+
+	public class Pagination
+	{
+		public int total_count { get; set; }
+		public int page { get; set; }
+		public int per_page { get; set; }
+		public int page_count { get; set; }
+		public int first { get; set; }
+		public int last { get; set; }
+	}
+
+	public class Paging
+	{
+		public int count { get; set; }
+		public int total { get; set; }
+		public int page { get; set; }
+		public int pages { get; set; }
+	}
+
+	public class Match
+	{
+		public string id { get; set; }
+		public int created { get; set; }
+		public int timestamp { get; set; }
+		public string name { get; set; }
+		public string title { get; set; }
+		public string mimetype { get; set; }
+		public string filetype { get; set; }
+		public string pretty_type { get; set; }
+		public string user { get; set; }
+		public bool editable { get; set; }
+		public int size { get; set; }
+		public string mode { get; set; }
+		public bool is_external { get; set; }
+		public string external_type { get; set; }
+		public bool is_public { get; set; }
+		public bool public_url_shared { get; set; }
+		public bool display_as_bot { get; set; }
+		public string username { get; set; }
+		public string url_private { get; set; }
+		public string url_private_download { get; set; }
+		public string thumb_64 { get; set; }
+		public string thumb_80 { get; set; }
+		public string thumb_360 { get; set; }
+		public int thumb_360_w { get; set; }
+		public int thumb_360_h { get; set; }
+		public string thumb_480 { get; set; }
+		public int thumb_480_w { get; set; }
+		public int thumb_480_h { get; set; }
+		public string thumb_160 { get; set; }
+		public string thumb_720 { get; set; }
+		public int thumb_720_w { get; set; }
+		public int thumb_720_h { get; set; }
+		public string thumb_800 { get; set; }
+		public int thumb_800_w { get; set; }
+		public int thumb_800_h { get; set; }
+		public string thumb_960 { get; set; }
+		public int thumb_960_w { get; set; }
+		public int thumb_960_h { get; set; }
+		public string thumb_1024 { get; set; }
+		public int thumb_1024_w { get; set; }
+		public int thumb_1024_h { get; set; }
+		public int original_w { get; set; }
+		public int original_h { get; set; }
+		public string thumb_tiny { get; set; }
+		public string permalink { get; set; }
+		public string permalink_public { get; set; }
+		public bool is_starred { get; set; }
+		public Shares shares { get; set; }
+		public object[] channels { get; set; }
+		public string[] groups { get; set; }
+		public object[] ims { get; set; }
+		public bool has_rich_preview { get; set; }
+		public string converted_pdf { get; set; }
+		public string thumb_pdf { get; set; }
+		public int thumb_pdf_w { get; set; }
+		public int thumb_pdf_h { get; set; }
+	}
+
+	public class Shares
+	{
+		public Private _private { get; set; }
+	}
+
+	public class Private
+	{
+		public GP3G0NW0L[] GP3G0NW0L { get; set; }
+		public G01HGAV7S4C[] G01HGAV7S4C { get; set; }
+	}
+
+	public class GP3G0NW0L
+	{
+		public object[] reply_users { get; set; }
+		public int reply_users_count { get; set; }
+		public int reply_count { get; set; }
+		public string ts { get; set; }
+		public string channel_name { get; set; }
+		public string team_id { get; set; }
+		public string share_user_id { get; set; }
+	}
+
+	public class G01HGAV7S4C
+	{
+		public object[] reply_users { get; set; }
+		public int reply_users_count { get; set; }
+		public int reply_count { get; set; }
+		public string ts { get; set; }
+		public string channel_name { get; set; }
+		public string team_id { get; set; }
+		public string share_user_id { get; set; }
+	}
+
+}
